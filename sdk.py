f12d3df604fce815c996529faf38a12a060b4e3b
Franco <168871500+SrFluff@users.noreply.github.com>
2024-06-07 14:19:28 -0400

    Added wait() getsys() and credits()

--- sdk.py
+++ sdk.py
@@ -1,5 +1,39 @@
 import os
 
+#Adds the time plugin, this allows us to wait a certain number of second
+
+import time
+
+#A more human-friendly version of time.sleep
+
+def wait(cc):
+
+    time.sleep(cc)
+
+#Prints the OS, for stuff like Linux, or MacOS it will return Unix-like instead of the OS name
+
+def getsys():
+
+    if os.name == "nt":
+
+        sys = "Windows"
+
+    else:
+
+        sys = "Unix-like"
+
+    print(sys)
+
+#Sets the SDK version
+
+sdkver = "1.0.0"
+
+#prints the current PBT version, made for providing credit
+
+def credits():
+
+    print("Basic Python Toolkit version " + sdkver + " By Franco M. (GitHub)")
+
 #Screen clearing
 
 def cls():
@@ -8,6 +42,8 @@ def cls():
     else:
         os.system("clear")
 cls()
+
+answer = "1"
 
 #Basic math functions
 
@@ -38,4 +74,20 @@ print("Welcome to the Basic Python Toolkit\n")
 
 hiw("You can highlight text by using hiw()")
 
-print(Back.WHITE + Fore.BLACK + hw + Fore.RESET + Back.RESET)
+print(Back.WHITE + Fore.BLACK + Fore.RESET + Back.RESET)
+
+print("You can get your OS by using getsys()\n")
+
+print("Use the wait() function to wait a certain about of seconds")
+
+wait(2)
+
+print("You are using a")
+
+getsys()
+
+print("system.\n")
+
+a = input("> ")
+
+exit()
