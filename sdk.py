b311255dd612ea9b72c5d1e10dcf1e67a334231f
Franco <168871500+SrFluff@users.noreply.github.com>
2024-06-07 12:08:59 -0400

    Added the main file

---
+++ sdk.py
@@ -0,0 +1,35 @@
+import os
+
+#Screen clearing
+
+def cls():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+cls()
+
+#Basic math functions
+
+def add(b, c):
+    print(b + c)
+def mul(d, e):
+    print(d * e)
+def sub(f, g):
+    print(f - g)
+def div(h, i):
+    print(h / i)
+
+#Colorama is the plugin required to color or highlight text
+
+from colorama import Back, Fore
+
+#Prints highlighted text
+
+def hiw(hw):
+
+# Resets the background color and text color
+
+    print(Back.WHITE + Fore.BLACK + hw + Fore.RESET + Back.RESET)
+
+cls()
